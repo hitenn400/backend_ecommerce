c72e3376abc715638ee0e7f7b592401d63a20284
hiten <hitenkakwani400@gmail.com>
2023-01-07 23:51:15 +0530

    update

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+// home route
 const {home,homeDummy} =require('../controllers/homeController');
 router.route('/').get(home);
 router.route('/dummy').get(homeDummy);
